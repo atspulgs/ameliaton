d20136dcc58a541fc1993bdd05893c6dbc6be745
Atspulgs <1491234+atspulgs@users.noreply.github.com>
2021-07-19 08:24:36 +0100

    card #2 completed

--- src/main/java/eu/keystruck/ameliaton/Ameliaton.java
+++ src/main/java/eu/keystruck/ameliaton/Ameliaton.java
@@ -38,35 +38,16 @@ public final class Ameliaton {
     
     public static void printHelp() {
         StringBuilder sb = new StringBuilder();
-        sb.append("\t").append(ARG_HELP).append(" | ").append(ARG_HELP_SHORT).append(" - ").append(DESC_HELP).append("\n");
+        sb.append("\t").append(ARG_HELP).append("\t  | ").append(ARG_HELP_SHORT).append(" - ").append(DESC_HELP).append("\n");
         sb.append("\t").append(ARG_VERSION).append(" | ").append(ARG_VERSION_SHORT).append(" - ").append(DESC_VERSION).append("\n");
         System.out.println(sb.toString());
     }
     
-    //This is where im working on the version as well as the pom file. I think the best is using java manifest in one way or another.
     public static void printVersion() {
         Package p = Ameliaton.class.getPackage();
         StringBuilder sb = new StringBuilder();
         sb.append("Implementation Title: ").append(p.getImplementationTitle()).append("\n");
-        sb.append("Implementation Vendor: ").append(p.getImplementationVendor()).append("\n");
         sb.append("Implementation Version: ").append(p.getImplementationVersion()).append("\n");
         System.out.println(sb.toString());
-        
-        // Another solution
-        /*Class clazz = MyClass.class;
-        String className = clazz.getSimpleName() + ".class";
-        String classPath = clazz.getResource(className).toString();
-        if (!classPath.startsWith("jar")) {
-          // Class not from JAR
-          return;
-        }
-        String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + 
-            "/META-INF/MANIFEST.MF";
-        Manifest manifest = new Manifest(new URL(manifestPath).openStream());
-        Attributes attr = manifest.getMainAttributes();
-        String value = attr.getValue("Manifest-Version");*/
-        //fetch version from the manifest. probbaly only possible when built.
-        //how should my code know if its run from IDE and when its fully built?
-        //we can leave this blank until we are ready to release the first version.
     }
 }
